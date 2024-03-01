cfb699ff2ed1b75f5b4a8f1378a0b4c501294392
Josh Burnett <joshuaburnett429@gmail.com>
2024-03-01 12:01:40 -0600

    established a base for card filtering and displaying

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import CardList from './CardGrabber';
-
+import { useState } from 'react';
 function App() {
+
+  const [selectedCardType, setSelectedCardType] = useState("all");
+  const [selectedAttribute, setSelectedAttribute] = useState("")
+
+  const handleChange = (event) => {
+    if(event.target.id === 'cardtype'){
+      setSelectedCardType(event.target.value)
+    }
+  }
+
+  const props = {selectedCardType, selectedAttribute}
+
   return (
     <div className="App">
       <header className="App-header">
-        <CardList/>
+        <label htmlFor='Card Type'>Select Card Type: </label>
+        <select id="cardtype" name="cardtype" defaultValue={selectedCardType} onChange={handleChange}>
+          <option value="all"></option>
+          <option value="Monster">Monster</option>
+          <option value="Spell">Spell</option>
+          <option value="Trap">Trap</option>
+        </select>
+        <CardList {...props}/>
       </header>
     </div>
   );
--- src/CardGrabber.js
+++ src/CardGrabber.js
@@ -1,32 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const CardList = () => {
+const CardList = (props) => {
+
+    
     const [cards, setCards] = useState([])
-    const params = {
-        cardset: 'Metal Raiders',
-        sort: 'atk'
-    }
+
+    const currentBanList = '2005-10-01';
+    
     useEffect(() => {
-        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php', {params})
+        axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php?&startdate=2000-01-01&enddate=${currentBanList}&dateregion=tcg`)
         .then(response => {
             // console.log('this is the console.log', response.data.data)
-            setCards(response.data.data)
+            
+            setCards([...response.data.data])
         })
         .catch(error => {
             console.error(error);
         });
     }, []);
 
+    console.log(props)
     console.log(cards)
 
     return cards.map(card => {
-        return (
-            <h2>{`${card.name}`}</h2>
-        )
+        if(card.type.includes(props.selectedCardType) || props.selectedCardType === "all"){
+            return (
+                <div className='cardBox'>
+                    <h2>{card.name}</h2>
+                    <h4>{card.desc}</h4>
+                    {card.atk && <p>atk: {card.atk}</p>}
+                    {card.def && <p>def: {card.def}</p>}
+                </div>
+            );
+        }
     })
+
 }
 
 export default CardList
 
+//props must contain all sorting functions. then pass props here and prepare the list.
+
 //pictures total size would be 1.6 gigs for all modern cards not including alternative card arts
+
+//Normal monsters atk highest to lowest
+//effect monsters same
+//ritual same
+//fusion same
+//spell
+//trap
